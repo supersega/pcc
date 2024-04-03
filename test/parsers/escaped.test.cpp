70a07283127b4172fbeb501a50651fa87dca1b6b
supersega <malyshev13091995@gmail.com>
2024-04-03 17:28:11 +0300

    add tests from nom example

--- test/parsers/escaped.test.cpp
+++ test/parsers/escaped.test.cpp
@@ -40,6 +40,23 @@ SCENARIO("escaped parser test happy path") {
         }
     }
 
+    GIVEN("examples from nom") {
+        WHEN("escaped parser applied") {
+            auto escaped_parser =
+                escaped(alphanumeric1(), '\\', one_of("\"n\\"));
+            auto parse_result = escaped_parser("123;");
+            REQUIRE(parse_result.is_success());
+            auto [value, rest] = parse_result.value_unsafe();
+            THEN("value is correct") { REQUIRE(value == "123"); }
+            THEN("rest is correct") { REQUIRE(rest == ";"); }
+            auto parse_result2 = escaped_parser(R"(12\"34;)");
+            REQUIRE(parse_result2.is_success());
+            auto [value2, rest2] = parse_result2.value_unsafe();
+            THEN("value is correct") { REQUIRE(value2 == R"(12\"34)"); }
+            THEN("rest is correct") { REQUIRE(rest2 == ";"); }
+        }
+    }
+
     GIVEN("empty string") {
         WHEN("escaped parser applied") {
             auto escaped_parser = escaped(alphanumeric1(), '\\', tag(";"));
@@ -59,7 +76,7 @@ SCENARIO("escaped parser test non happy path") {
             REQUIRE(parse_result.error_unsafe() == "Not escapable symbol");
         }
     }
-    
+
     GIVEN("string with escape symbol at the end") {
         WHEN("escaped parser applied") {
             auto escaped_parser = escaped(alphanumeric1(), '\\', tag(";"));
@@ -68,14 +85,14 @@ SCENARIO("escaped parser test non happy path") {
             REQUIRE(parse_result.error_unsafe() == "End with control symbol");
         }
     }
-    
+
     GIVEN("string with escape symbol at the end") {
         WHEN("escaped parser applied") {
             auto escaped_parser = escaped(alphanumeric1(), '\\', tag(";"));
             auto parse_result = escaped_parser("#123");
             REQUIRE(!parse_result.is_success());
-            REQUIRE(parse_result.error_unsafe() == "Non normal or control symbol");
+            REQUIRE(parse_result.error_unsafe() ==
+                    "Non normal or control symbol");
         }
     }
 }
-
